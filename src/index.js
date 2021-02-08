d32e0d7bc161b09e3c1450b8f63317a3f1210da9
Your Username <you@example.com>
2021-02-08 14:20:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+ 
+  
+  const task = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", createNewTask); 
 });
+  
+  
+  const createNewTask = evt => {
+    evt.preventDefault();
+    const taskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = taskDescription.value;
+
+    appendNewTask(newTask);
+    evt.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task)
+};
